a9c2ba39cc8e77c9ddd5146eb7a81f3d7292e496
Matt <mattkubota5@gmail.com>
2017-08-20 16:01:28 -0400

    updated seed file and added a Store controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root 'products#index'
+  root 'store#index', as: 'store_index'
   get 'about' => 'pages#about', as: :about
-  get 'contact' => 'pages#contact', as: :contact 
+  get 'contact' => 'pages#contact', as: :contact
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
