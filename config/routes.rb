13f11805f02b2b1f0e0336ba0f340f3639a0c399
Matt <mattkubota5@gmail.com>
2017-08-20 14:31:07 -0400

    created navbar and added links

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'products#index'
+  get 'about' => 'pages#about', as: :about
+  get 'contact' => 'pages#contact', as: :contact 
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
