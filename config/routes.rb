6a1eff18af5a02c4137a6b6b91b3c0e252c3c25c
Matt <mattkubota5@gmail.com>
2017-08-29 21:11:27 -0400

    created user and admin functionality with the proper views and forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  root 'store#index', as: 'store_index'
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+  resources :users
   resources :orders
   resources :line_items
   resources :carts
-  root 'store#index', as: 'store_index'
   get 'about' => 'pages#about', as: :about
   get 'contact' => 'pages#contact', as: :contact
   resources :products
